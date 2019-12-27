a624a62273ca4c053a77b6620c34c7050fb9954e
Ovidiu Fosalau <ovidiu.fosalau@gmail.com>
2019-12-27 17:53:35 +0200

    added repositories display

--- ui.js
+++ ui.js
@@ -72,4 +72,27 @@ class UI {
             this.clearAlert();
         }, 3000);
     }
+
+    // Show the found user repositories
+    showRepos(repos){
+        let output = '';
+        repos.forEach((repo) => {
+            output += `
+                <div class="card card-body mb-2">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                        </div>
+                        <div class="col-md-6">
+                            <span class="badge badge-primary">Stars: ${repo.stargazers_count}</span> 
+                            <span class="badge badge-secondary">Watchers: ${repo.watchers_count}</span> 
+                            <span class="badge badge-success">Forks: ${repo.forks_count}</span> 
+                        </div>
+                    </div>
+                </div>
+            `;
+        });
+        // output repositories
+        document.getElementById('repos').innerHTML = output;
+    }
 }
